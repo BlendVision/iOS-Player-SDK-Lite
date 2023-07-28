61eefdee75b56d31354f7e1be8f9cb01c22783f0
Jay Lo <131951442+jaylo1979@users.noreply.github.com>
2023-07-28 14:59:48 +0800

    Update Package.swift
    Update url & checksum

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "BVPlayerCore",
-            url: "https://github.com/BlendVision/iOS-Player-SDK/releases/download/2.4.0/KKSPlayer-2.4.0.41.xcframework.zip",
-            checksum: "205e980dab41f311359725dbaf9e3ad6f127ca5e5981b1c7a690c9c82ba6dd9d"
+            url: "https://github.com/BlendVision/iOS-Player-SDK/releases/download/2.5.0/KKSPlayer-2.5.0.43-WOPSE.xcframework.zip",
+            checksum: "8ec3e93f9b296dab476243357d3c3dfba0d33cabee20fa42dbf9c08c72646b70"
         ),
         .binaryTarget(
             name: "SwiftWebVTT",
