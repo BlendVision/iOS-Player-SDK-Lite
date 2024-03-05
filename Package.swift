a1cb07a35343a51905c4a55af68917aefe5935e2
Jay Lo <jaylo@kkstream.com>
2024-03-05 09:24:54 +0800

    Update the Package file due to the name of repository was changed

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "BVPlayerCore",
+    name: "BVPlayerLite",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BVPlayerCore",
-            targets: ["BVPlayerCore"]),
+            name: "BVPlayerLite",
+            targets: ["BVPlayerLite"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,8 +21,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "BVPlayerCore",
-            url: "https://github.com/BlendVision/bvplayer-ios-core/releases/download/3.0.0/BVPlayer-3.0.0.7953271_.WOPSE_Release.xcframework.zip",
+            name: "BVPlayerLite",
+            url: "https://github.com/BlendVision/iOS-Player-SDK-Lite/releases/download/3.0.0/BVPlayer-3.0.0.7953271_.WOPSE_Release.xcframework.zip",
             checksum: "9fde22ea0514f53058f2fba38156bde6f1e282d9400694c5570b0ebe6fc7d87b"
         )
     ]
